1c296a35c96d9727d6a7a63912b5e44542ace6ff
Dmitry Sidorov <viwet42@gmail.com>
2024-11-19 01:13:25 +0100

    test(eip2333): test lamport_0 and lamport_1 calculation

--- derive_test.go
+++ derive_test.go
@@ -1,9 +1,12 @@
 package gobls12381
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"os"
+	"slices"
+	"strings"
 	"testing"
 )
 
@@ -43,4 +46,100 @@ func Test_deriveMasterKey(t *testing.T) {
 		seed, key := v[0], v[1]
 		f(t, seed, key)
 	}
+}
+
+func Test_deriveLamport0(t *testing.T) {
+	f := func(t *testing.T, seedHex string, index uint32, wantedLamport []string) {
+		seed, err := hex.DecodeString(seedHex)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		masterSecretKey, err := deriveMasterSecretKey(seed)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		salt := make([]byte, 4)
+		binary.BigEndian.PutUint32(salt, index)
+
+		lamport0, err := deriveLamport0(masterSecretKey, salt)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if slices.CompareFunc(
+			lamport0,
+			wantedLamport,
+			func(a []byte, b string) int { return strings.Compare(hex.EncodeToString(a), b) },
+		) != 0 {
+			t.Fatal("Invalid lamport0 calculation")
+		}
+	}
+
+	var test struct {
+		Seed     string   `json:"seed"`
+		Index    uint32   `json:"index"`
+		Lamport0 []string `json:"lamport_0"`
+	}
+
+	file, err := os.Open("tests/derive_lamport_0.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	if err := json.NewDecoder(file).Decode(&test); err != nil {
+		t.Fatal(err)
+	}
+
+	f(t, test.Seed, test.Index, test.Lamport0)
+}
+
+func Test_deriveLamport1(t *testing.T) {
+	f := func(t *testing.T, seedHex string, index uint32, wantedLamport []string) {
+		seed, err := hex.DecodeString(seedHex)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		masterSecretKey, err := deriveMasterSecretKey(seed)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		salt := make([]byte, 4)
+		binary.BigEndian.PutUint32(salt, index)
+
+		lamport1, err := deriveLamport1(masterSecretKey, salt)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if slices.CompareFunc(
+			lamport1,
+			wantedLamport,
+			func(a []byte, b string) int { return strings.Compare(hex.EncodeToString(a), b) },
+		) != 0 {
+			t.Fatal("Invalid lamport1 calculation")
+		}
+	}
+
+	var test struct {
+		Seed     string   `json:"seed"`
+		Index    uint32   `json:"index"`
+		Lamport1 []string `json:"lamport_1"`
+	}
+
+	file, err := os.Open("tests/derive_lamport_1.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	if err := json.NewDecoder(file).Decode(&test); err != nil {
+		t.Fatal(err)
+	}
+
+	f(t, test.Seed, test.Index, test.Lamport1)
 }
